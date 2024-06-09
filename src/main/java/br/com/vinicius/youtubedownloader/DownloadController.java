2334aa2e27ebe3a8f8bb5633a0c60f0c1a993483
Vinicius Gomes <142799171+ViniciusG03@users.noreply.github.com>
2024-06-08 22:34:55 -0300

    feat: update DownloadController

--- src/main/java/br/com/vinicius/youtubedownloader/DownloadController.java
+++ src/main/java/br/com/vinicius/youtubedownloader/DownloadController.java
@@ -1,13 +1,18 @@
 package br.com.vinicius.youtubedownloader;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 
 public class DownloadController {
+    private String savePath = System.getProperty("user.home");
+
     @FXML
     private TextField urlField;
 
@@ -20,13 +25,25 @@ public class DownloadController {
     @FXML
     protected void handleDownloadButtonAction() {
         String url = urlField.getText();
+
         if (url.isEmpty()) {
             statusLabel.setText("Por favor, insira uma URL.");
             return;
         }
 
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Escolher Caminho");
+        File selectedDirectory = directoryChooser.showDialog(downloadButton.getScene().getWindow());
+
+        if (selectedDirectory != null) {
+            savePath = selectedDirectory.getAbsolutePath();
+        } else {
+            statusLabel.setText("Por favor, escolha um caminho para salvar o vídeo.");
+            return;
+        }
+
         try {
-            ProcessBuilder builder = new ProcessBuilder("C:\\Program Files\\youtube-dlp\\yt-dlp.exe", "-o", "%(title)s.%(ext)s", "-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]", url);
+            ProcessBuilder builder = new ProcessBuilder("yt-dlp.exe", "-o", savePath + "/%(title)s.%(ext)s", "-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]", "mp4", url);
             builder.redirectErrorStream(true);
             Process process = builder.start();
 
@@ -48,3 +65,5 @@ public class DownloadController {
         }
     }
 }
+
+
--- src/main/java/br/com/vinicius/youtubedownloader/Main.java
+++ src/main/java/br/com/vinicius/youtubedownloader/Main.java
@@ -1,6 +1,7 @@
 package br.com.vinicius.youtubedownloader;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,8 +12,16 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         try {
             Parent root = FXMLLoader.load(getClass().getResource("/br/com/vinicius/youtubedownloader/download_view.fxml"));
+            Scene scene = new Scene(root, 600, 400);
+
+            scene.getStylesheets().add(getClass().getResource("/dark-theme.css").toExternalForm());
+
             primaryStage.setTitle("YouTube Video Downloader");
-            primaryStage.setScene(new Scene(root, 600, 400));
+            primaryStage.setScene(scene);
+
+            Image icon = new Image(getClass().getResourceAsStream("/src/main/resources/yt-logo.png"));
+            primaryStage.getIcons().add(icon);
+
             primaryStage.show();
         } catch(Exception e) {
             e.printStackTrace();
